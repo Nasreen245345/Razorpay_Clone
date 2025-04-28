08ea506b5ea6112f84a8daee39476fd38def073b
Nasreen245345 <fatimanasreen950@gmail.com>
2025-04-28 19:39:12 +0500

    Upload Razorpay Clone Project Code

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        deepBlue: '#02042a', // Razorpay-like dark blue
+      },
+    },
+  },
+};
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          mullish: ['Mullish', 'sans-serif'],
+        },
+      },
+    },
+  };
+  module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          mulish: ['Mulish', 'sans-serif'],
+        },
+      },
+    },
+  }
+   
